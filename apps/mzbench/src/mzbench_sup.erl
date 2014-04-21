bcd38c04235ad9f7389c1cc1a46d226e66bb14a2
Dmitry Ivanov <ethercrow@gmail.com>
2014-04-21 16:51:07 +0700

    WIP on director.

--- apps/mzbench/src/mzbench_sup.erl
+++ apps/mzbench/src/mzbench_sup.erl
@@ -1,15 +1,15 @@
 -module(mzbench_sup).
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([start_link/1]).
 -export([init/1]).
 
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(ScriptFileName) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, [ScriptFileName]).
 
-init([]) ->
+init([ScriptFileName]) ->
     {ok, { {one_for_one, 5, 10},
     [
         {workers, {mzbench_worker_sup, start_link, []}, permanent, 5000, supervisor, [mzbench_worker_sup]}
