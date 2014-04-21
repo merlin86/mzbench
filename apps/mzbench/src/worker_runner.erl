17aafee4fb96de82b5d22f98abb0534af03d47e3
Renat Idrisov <ridrisov@machinezone.com>
2014-04-21 14:18:06 +0700

    bench-4 loop construct: rate, time

--- apps/mzbench/src/worker_runner.erl
+++ apps/mzbench/src/worker_runner.erl
@@ -12,16 +12,19 @@
 %% Feel free to add when necessary.
 -spec run_worker_script(spec(), [expr()], module()) -> {ok, state()}.
 run_worker_script(_Spec, Script, WorkerModule) ->
-    WorkerResultState = lists:foldl(fun(Expr, S) ->
-                                            element(2, eval_expr(Expr, S, WorkerModule))
-                                    end,
-                                    WorkerModule:initial_state(),
-                                    Script),
+    {_, WorkerResultState} = eval_expr(Script, WorkerModule:initial_state(), WorkerModule),
     {ok, WorkerResultState}.
 
 -spec eval_expr(expr(), state(), module()) -> {value(), state()}.
 eval_expr(ExprTuple, State, WorkerModule) when is_tuple(ExprTuple) ->
     eval_function(tuple_to_list(ExprTuple), State, WorkerModule);
+eval_expr(ExprList, State, WorkerModule) when is_list(ExprList) ->
+  lists:foldl(fun(E, {EvaluatedParams, CurrentState}) ->
+                {EP, S} = eval_expr(E, CurrentState, WorkerModule),
+                {EvaluatedParams ++ [EP], S}
+              end,
+              {[], State},
+              ExprList);
 eval_expr(Value, State, _) -> {Value, State}.
 
 %% TODO: Implement builtin special forms and functions in separate module
@@ -29,17 +32,41 @@ eval_expr(Value, State, _) -> {Value, State}.
 %% This way eval_functions will be concerned with just evaluating
 %% function parameters and dispatching.
 -spec eval_function([expr()], state(), module()) -> {value(), state()}.
-eval_function([loop, _LoopSpec, Body], State, WorkerModule) ->
-    {nil, lists:foldl(fun(E, S) -> eval_expr(E, S, WorkerModule) end,
-                      State,
-                      Body)};
+eval_function([loop, LoopSpec, Body], State, WorkerModule) ->
+  {Rate, rps} = proplists:get_value(rate, LoopSpec, {undefined, rps}),
+  {Time, ms} = proplists:get_value(time, LoopSpec, {undefined, ms}),
+  Interval = if
+               Rate == undefined -> 0;
+               Rate == 0 -> undefined;
+               true -> 1000/Rate
+             end,
+  if
+    Interval == undefined -> {nil, State};
+    true -> timerun(msnow(), Time, trunc(Interval), Body, State, WorkerModule)
+  end;
+eval_function([choose, List], _, _) -> utility:choose(List);
+eval_function([choose, N, List], _, _) -> utility:choose(N, List);
+eval_function([random_bytes, N], _, _) -> utility:random_bytes(N);
 eval_function([FnName | ParamExprs], State, WorkerModule) ->
     %% Eager left-to-right evaluation of parameters.
-    {Params, NextState} = lists:foldl(fun(E, {EvaluatedParams, CurrentState}) ->
-                                              {EP, S} = eval_expr(E, CurrentState, WorkerModule),
-                                              {EvaluatedParams ++ [EP], S}
-                                      end,
-                                      {[], State},
-                                      ParamExprs),
+    {Params, NextState} = eval_expr(ParamExprs, State, WorkerModule),
     apply(WorkerModule, FnName, [NextState | Params]).
 
+
+msnow() ->
+  {MegaSecs, Secs, MicroSecs} = erlang:now(),
+  trunc(MegaSecs * 1000000000 + Secs * 1000 + MicroSecs / 1000).
+
+timerun(Start, Time, Interval, Expr, State, WorkerModule) ->
+  LocalStart = msnow(),
+  if
+    (Time =/= undefined) and (Time + Start =< LocalStart) -> {nil, State};
+    true ->
+      {_, NextState} = eval_expr(Expr, State, WorkerModule),
+      Remain = LocalStart + Interval - msnow(),
+      if
+        Remain > 0 -> timer:sleep(Remain);
+        true -> ok
+      end,
+    timerun(Start, Time, Interval, Expr, NextState, WorkerModule)
+  end.
--- apps/mzbench/src/worker_runner_tests.erl
+++ apps/mzbench/src/worker_runner_tests.erl
@@ -13,5 +13,29 @@ print_three_test() ->
     Script = [{print, "FOO"}, {print, "BAR"}, {print, "BAZ"}],
     ?assertEqual({ok, "FOOBARBAZ"}, run(Script)).
 
+print_loop_test() ->
+    Script = [{loop, [{time, {1100, ms}},
+                      {rate, {4, rps}}],
+               [{print, "F"}]}],
+    ?assertEqual({ok, "FFFFF"}, run(Script)).
+
+empty_loop_test() ->
+    Script = [{loop, [{time, {0, ms}},
+                      {rate, {4, rps}}],
+               [{print, "FOO"}]}],
+    ?assertEqual({ok, ""}, run(Script)).
+
+empty_loop2_test() ->
+    Script = [{loop, [{time, {1100, ms}},
+                      {rate, {4, rps}}],
+               []}],
+    ?assertEqual({ok, ""}, run(Script)).
+
+empty_loop3_test() ->
+    Script = [{loop, [{time, {1100, ms}},
+                      {rate, {0, rps}}],
+               [{print, "FOO"}]}],
+    ?assertEqual({ok, ""}, run(Script)).
+
 run(Script) ->
     worker_runner:run_worker_script(0, Script, dummy_worker).
