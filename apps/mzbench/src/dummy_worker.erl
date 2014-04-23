0cbf6ba91153699071e96e7b2c2b29c76efa63c7
Renat Idrisov <ridrisov@machinezone.com>
2014-04-23 15:00:15 +0700

    folsom support for dummy worker

--- apps/mzbench/src/dummy_worker.erl
+++ apps/mzbench/src/dummy_worker.erl
@@ -4,9 +4,10 @@
          print/2]).
 
 -spec initial_state() -> string().
-initial_state() -> "".
+initial_state() -> folsom_metrics:new_history(printed),"".
 
 -spec print(string(), string()) -> {nil, string()}.
 print(State, Line) ->
+    folsom_metrics:notify({printed, Line}),
     lager:info("Appending ~p", [Line]),
     {nil, State ++ Line}.
