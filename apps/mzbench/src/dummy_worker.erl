8010eceb2e86f03c21de8ca3ce000a8388e46b79
Renat Idrisov <ridrisov@machinezone.com>
2014-04-24 16:54:12 +0700

    dummy_worker metric name update

--- apps/mzbench/src/dummy_worker.erl
+++ apps/mzbench/src/dummy_worker.erl
@@ -4,11 +4,19 @@
          print/3]).
 
 -spec initial_state() -> string().
-initial_state() -> folsom_metrics:new_history(printed),"".
+initial_state() -> "".
 
 -spec print(string(), string(), list()) -> {nil, string()}.
 print(State, Text, Meta) ->
-    Line = proplists:get_value(line, Meta),
-    folsom_metrics:notify({printed, {Line, Text}}),
+    Line = proplists:get_value(line, Meta, 0),
+    notify(string:concat(integer_to_list(Line), "-print"), Text),
     lager:info("Appending ~p, Meta: ~p~n", [Text, Meta]),
     {nil, State ++ Text}.
+
+notify(Metric, Value) ->
+  Exists =lists:any(fun(E)-> E == Metric end, folsom_metrics:get_metrics()),
+  if
+    Exists == false -> folsom_metrics:new_history(Metric);
+    true -> ok
+  end,
+  folsom_metrics:notify({Metric, Value}).
