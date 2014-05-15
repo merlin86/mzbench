4e58d4c91b934dcce833db7cedcd59bfd3fc2df2
Renat Idrisov <ridrisov@machinezone.com>
2014-05-15 17:52:35 +0700

    bench-15 extra merging issues

--- apps/mzbench/src/metrics.erl
+++ apps/mzbench/src/metrics.erl
@@ -2,19 +2,31 @@
 
 -export([notify_counter/1, notify_roundtrip/2]).
 
+
+notify(Metric, Value, Type, 1) -> folsom_metrics:notify(Metric, Value, Type);
+notify(Metric, Value, Type, SampleMetrics) ->
+  case random:uniform() of
+    X when X<SampleMetrics -> folsom_metrics:notify(Metric, Value, Type);
+    _ -> ok
+  end.
+
 notify_counter(Meta) ->
+    SampleMetrics = proplists:get_value(sample_metrics, Meta, 1),
     Prefix = extract_metric_prefix(Meta),
-    folsom_metrics:notify(
+    notify(
                 Prefix ++ ".counter",
                 {inc, 1},
-                counter).
+                counter,
+                SampleMetrics).
 
 notify_roundtrip(Meta, Value) ->
+    SampleMetrics = proplists:get_value(sample_metrics, Meta, 1),
     Prefix = extract_metric_prefix(Meta),
-    folsom_metrics:notify(
+    notify(
       Prefix ++ ".roundtrip",
       Value,
-      histogram).
+      histogram,
+      SampleMetrics).
 
 extract_metric_prefix(Meta) ->
     Prefix = proplists:get_value(metric_prefix, Meta),
