a2bb4d32910fe9392209a4ceda5bc0b6c04a6caa
Dmitry Ivanov <divanov@machinezone.com>
2014-06-25 18:08:05 +0100

    Fix sudo not working in bench.py.

--- cluster/bench.py
+++ cluster/bench.py
@@ -13,6 +13,9 @@ from cluster_plan import allocate, deallocate
 from process import run_local
 from util import log
 
+def info(msg):
+    log('[ INFO ] ' + msg)
+
 def check_ansible_result(res):
     failures = {}
     for host, result in res['dark'].iteritems():
@@ -44,6 +47,14 @@ def ensure_file(inv, path, content, owner='root', group='root', mode='400'):
                                       'group':   group,
                                       'mode':    mode })
 
+def ensure_dir(inv, path, owner='root'):
+    return run_ansible(inv, 'file',
+            { 'path': path,
+                'state':   'directory',
+                'owner':   'root',
+                'group':   'root'})
+
+
 def rand_str(len=20, chars=string.ascii_uppercase):
     return ''.join(random.choice(chars) for _ in range(len))
 
@@ -57,21 +68,28 @@ def allocate_hosts(nodes_count, purpose):
                                         'quantity':       nodes_count
                                    }}]})
     hosts = services[purpose]
-    print "[ OK ] Allocated %s hosts for '%s': %s" % (nodes_count, purpose, hosts)
+    info("Allocated %s hosts for '%s': %s" % (nodes_count, purpose, hosts))
     return hosts
 
 def setup_bench(hosts, cookie):
     inv = ansible.inventory.Inventory(hosts)
     ensure_file(inv, '/root/.erlang.cookie', cookie)
+    ensure_dir(inv, '/etc/sudoers.d')
+    ensure_file(inv, '/etc/sudoers.d/root', 'Defaults:root !requiretty\n')
+
     run_ansible(inv, 'yum',     {'name': 'mzbench', 'state': 'present'})
+    info("mzbench rpm is present")
+
     run_ansible(inv, 'service', {'name': 'mzbench', 'state': 'started'})
+    info("mzbench is started")
+
     run_local(["../wait_cluster_start", cookie, '10000'] + hosts)
-    print "[ OK ] mzbench nodes ready: %s" % hosts
+    info("mzbench nodes ready: %s" % hosts)
 
 def run_bench(script, host, cookie):
-    print "Running '%s' on %s" % (script, host)
+    info("Running '%s' on %s" % (script, host))
     run_local(["../run", script, cookie, host])
-    print "[ OK ] Finished '%s' on %s" % (script, host)
+    info("[ OK ] Finished '%s' on %s" % (script, host))
 
 if __name__ == "__main__":
     import argparse
@@ -85,9 +103,20 @@ if __name__ == "__main__":
         purpose = "bench-" + rand_str(len=10)
         cookie  = rand_str(len=20)
 
+        info("user: {0}".format(user))
+        info("purpose: {0}".format(purpose))
+        info("cookie: {0}".format(cookie))
+
+        info("Allocating hosts")
         hosts = allocate_hosts(args['nodes_count'], purpose)
+        info("Got hosts: {0}".format(hosts))
+
+        info("Running setup")
         setup_bench(hosts, cookie)
+
+        info("Running script")
         run_bench(args['script'][0], hosts[0], cookie)
     except Exception as e:
         log("[ ERROR ] Failed to allocate boxes, rolling back")
+        log(e)
     deallocate(purpose)
