c732a30523a5c4fbcd854e11b269f9844b02dce7
Timofey Barmin <tbarmin@machinezone.com>
2015-12-08 16:27:21 +0600

    Add --jobs arg into migrator

--- bin/migrate.py
+++ bin/migrate.py
@@ -2,13 +2,15 @@
 """mzbench data migrator
 
 Usage:
-  migrate [--check] <data_dir>
+  migrate [--check] [--jobs <jobs>] <data_dir>
 
 Options:
     <data_dir>
                     Path to MZBench data directory
     --check
-                     Check for actual migrations
+                    Check for actual migrations
+    --jobs <jobs>
+                    Number of parallel jobs, by default set to number of cores
 """
 
 from __future__ import print_function
@@ -20,6 +22,7 @@ from contextlib import contextmanager
 from os import path
 from distutils.dir_util import mkpath
 import shutil
+import multiprocessing
 
 dirname = path.dirname(path.realpath(__file__))
 migrations_dir = path.join(dirname, '../migrations/')
@@ -64,7 +67,7 @@ def migrations_to_apply(data_dir):
         print("Nothing to migrate")
     return to_apply
 
-def migrate(data_dir):
+def migrate(data_dir, jobs):
     to_apply = migrations_to_apply(data_dir)
     keys = to_apply.keys()
     keys.sort()
@@ -73,8 +76,8 @@ def migrate(data_dir):
         print('================================================================================')
         print('Migration: {0}'.format(to_apply[i]))
         print('================================================================================')
-        with backup(i, data_dir):
-            apply_migration(i, to_apply[i], data_dir)
+        with backup(i, data_dir, jobs):
+            apply_migration(to_apply[i], data_dir, jobs)
             save_state(i, data_dir)
 
 def benchmarks(data_dir):
@@ -82,42 +85,48 @@ def benchmarks(data_dir):
         if d != ".migrations":
             yield d
 
-def apply_migration(i, script_path, data_dir):
-    for bench_id in benchmarks(data_dir):
-        bench_path = path.join(data_dir, bench_id)
-        cmd = [script_path, bench_path]
-        cmd_str = " ".join(cmd)
-        print('Executing {0}'.format(cmd_str))
-        try:
-            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            res_out, res_err = p.communicate()
-        except:
-            print("Unexpected error: {0}".format(sys.exc_info()))
-            raise
+def apply_migration(script_path, data_dir, jobs):
+    pool = multiprocessing.Pool(processes=jobs)
+    pool.map(migration_worker, [ (i, script_path, data_dir) for i in benchmarks(data_dir) ], 1)
 
-        if p.returncode != 0:
-            print('ERROR: Migration "{0}" returned {1}\nOutput: {2}\nStderr: {3}\n'.format(cmd_str, p.returncode, res_out, res_err))
-            raise Exception('Migration "{0}" failed'.format(cmd_str))
+def migration_worker((bench_id, script_path, data_dir)):
+    bench_path = path.join(data_dir, bench_id)
+    cmd = [script_path, bench_path]
+    cmd_str = " ".join(cmd)
+    print('Executing {0} in {1}'.format(cmd_str, os.getpid()))
+    try:
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        res_out, res_err = p.communicate()
+        return res_err
+    except:
+        print("Unexpected error: {0}".format(sys.exc_info()))
+        raise
+
+    if p.returncode != 0:
+        print('ERROR: Migration "{0}" returned {1}\nOutput: {2}\nStderr: {3}\n'.format(cmd_str, p.returncode, res_out, res_err))
+        raise Exception('Migration "{0}" failed'.format(cmd_str))
 
 @contextmanager
-def backup(name, data_dir):
-    do_backup(name, data_dir)
+def backup(name, data_dir, jobs):
+    do_backup(name, data_dir, jobs)
     try:
         yield
     except:
         do_rollback(name, data_dir)
         raise
 
-def do_backup(name, data_dir):
+def do_backup(name, data_dir, jobs):
     backupdir = os.path.join(data_dir, '.migrations', 'backup', '{0}'.format(name))
     mkpath(backupdir)
     print('Making a backup {0}'.format(backupdir))
-    for bench_id in benchmarks(data_dir):
-        from_dir = path.join(data_dir, bench_id)
-        to_dir = path.join(backupdir, bench_id)
-        print('.', end='')
-        shutil.copytree(from_dir, to_dir)
+    pool = multiprocessing.Pool(processes=jobs)
+    pool.map(backup_dir, [(i, data_dir, backupdir) for i in benchmarks(data_dir)])
     print()
+
+def backup_dir((bench_id, data_dir, backupdir)):
+    from_dir = path.join(data_dir, bench_id)
+    to_dir = path.join(backupdir, bench_id)
+    shutil.copytree(from_dir, to_dir)
 
 def do_rollback(name, data_dir):
     backupdir = os.path.join(data_dir, '.migrations', 'backup', '{0}'.format(name))
@@ -134,11 +143,19 @@ def do_rollback(name, data_dir):
 def main():
     args = docopt.docopt(__doc__, version='0.1.0')
     data_dir = args['<data_dir>']
+
+    if args['--jobs']:
+        jobs = int(args['--jobs'])
+    else:
+        jobs = multiprocessing.cpu_count()
+
+    print("Jobs: {0}".format(jobs))
+
     if args['--check']:
         if migrations_to_apply(data_dir):
             sys.exit(1)
     else:
-        migrate(data_dir)
+        migrate(data_dir, jobs)
 
 if __name__ == '__main__':
     main()
